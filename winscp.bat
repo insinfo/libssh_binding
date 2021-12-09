061ff14cd7c737d4f1a227f89d40c8e3e32ae06a
Isaque Neves <insinfo2008@gmail.com>
2021-12-09 11:43:30 -0300

    clean

--- winscp.bat
+++
@@ -1,2 +0,0 @@
-winscp.com /ini=nul /command "open sftp://isaque.neves:Ins257257@192.168.133.13/ -hostkey=""*""" "get ""/home/isaque.neves/go1.11.4.linux-amd64.tar.gz"" ""D:\MyDartProjects\fsbackup\libssh_binding\""" "exit"
-
